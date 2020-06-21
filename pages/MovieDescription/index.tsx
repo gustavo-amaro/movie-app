26ee3937ab46bc508aea945993a21b09dd021f55
Gustavo Amaro <gustavo.amaro97@gmail.com>
2020-06-20 23:07:00 -0300

    feat: show movie actors

--- pages/MovieDescription/index.tsx
+++ pages/MovieDescription/index.tsx
@@ -1,62 +1,116 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Header from '../../components/Header';
-import { Container, Description, Title, MovieTitle, Rate, ButtonBack } from './styles';
-import { Linking, TouchableHighlight, Text } from 'react-native';
-import Content from './components/Content';
-import api from '../../services/api';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Header from "../../components/Header";
+import {
+  Container,
+  Description,
+  Title,
+  MovieTitle,
+  Rate,
+  ButtonBack,
+  ActorWrapper,
+  ActorsContainer,
+  ActorImage,
+  ActorName,
+  ActorCharacter,
+} from "./styles";
+import { Linking, TouchableHighlight, Text } from "react-native";
+import Content from "./components/Content";
+import api from "../../services/api";
+import Icon from "react-native-vector-icons/FontAwesome";
 
-function MovieDescription({ route, navigation }){
-    const imageUrl = 'https://image.tmdb.org/t/p/w500';
-    const { movieId } = route.params;
-    const [movie, setMovie] = useState({});
+function MovieDescription({ route, navigation }) {
+  const imageUrl = "https://image.tmdb.org/t/p/w500";
+  const { movieId } = route.params;
+  const [movie, setMovie] = useState({});
+  const [actors, setActors] = useState([]);
 
-    useEffect(()=>{
-        async function getMovie(){
-            const response = await api.get(`/movie/${movieId}?&language=pt-BR`);
-            setMovie(response.data);
-        }
-        getMovie();
-    }, []);
-
-    async function openHomepage(){
-        const supported = await Linking.canOpenURL(movie.homepage);
-        if(supported){
-            await Linking.openURL(movie.homepage);
-        }
+  useEffect(() => {
+    async function getMovie() {
+      const response = await api.get(`/movie/${movieId}?&language=pt-BR`);
+      setMovie(response.data);
     }
-    return(
-        <Container>
-            <Header image={imageUrl+movie.backdrop_path} />
-            <ButtonBack onPress={()=>navigation.goBack()}>
-                <Icon name="angle-left" color={'#fff'} size={22}/>
-            </ButtonBack>
-            <MovieTitle>{movie.title + ' - '+ (new Date(movie.release_date).getFullYear())}</MovieTitle>
-            <Rate><Icon name="star" color={"#ff0"} size={20}/> {movie.vote_average}</Rate>
-            <Content>
-                <Title>Sinopse:</Title>
-                <Description>{movie.overview}</Description>
-                
-                <Title>Título original:</Title>
-                <Description>{movie.original_title}</Description>
-                
-                <Title>Data de lançamento: </Title>
-                <Description>{new Date(movie.release_date).toLocaleDateString()}</Description>
+    async function getActors() {
+      const actors = (await api.get(`/movie/${movieId}/credits?language=pt-BR`))
+        .data.cast;
+      setActors(actors.slice(0, 12));
+    }
+    getMovie();
+    getActors();
+  }, []);
 
-                <Title>Gêneros: </Title>
-                <Description>
-                    {movie.genres && movie.genres.map(gen=><React.Fragment key={gen.id}> - {gen.name}</React.Fragment>)}
-                </Description>
+  async function openHomepage() {
+    const supported = await Linking.canOpenURL(movie.homepage);
+    if (supported) {
+      await Linking.openURL(movie.homepage);
+    }
+  }
+  return (
+    <Container>
+      <Header image={imageUrl + movie.backdrop_path} />
+      <ButtonBack onPress={() => navigation.goBack()}>
+        <Icon name="angle-left" color={"#fff"} size={22} />
+      </ButtonBack>
+      <MovieTitle>
+        {movie.title + " - " + new Date(movie.release_date).getFullYear()}
+      </MovieTitle>
+      <Rate>
+        <Icon name="star" color={"#ff0"} size={20} /> {movie.vote_average}
+      </Rate>
+      <Content>
+        {movie.overview !== "" && (
+          <>
+            <Title>Sinopse:</Title>
+            <Description>{movie.overview}</Description>
+          </>
+        )}
+        <Title>Título original:</Title>
+        <Description>{movie.original_title}</Description>
 
-                <Title>Homepage: </Title>
-                <TouchableHighlight onPress={openHomepage}>
-                    <Description color={'#00f'}>{movie.homepage}</Description>
-                </TouchableHighlight>
+        <Title>Data de lançamento: </Title>
+        <Description>
+          {new Date(movie.release_date).toLocaleDateString()}
+        </Description>
 
-            </Content>
-        </Container>
-    )
+        <Title>Gêneros: </Title>
+        <Description>
+          {movie.genres &&
+            movie.genres.map((gen, index) => {
+              if (index !== movie.genres.length - 1) {
+                return (
+                  <React.Fragment key={gen.id}>{gen.name} - </React.Fragment>
+                );
+              } else {
+                return <React.Fragment key={gen.id}>{gen.name}</React.Fragment>;
+              }
+            })}
+        </Description>
+
+        <Title>Atores:</Title>
+        <ActorsContainer>
+          {actors.map((actor) => (
+            <ActorWrapper key={actor.id}>
+              <ActorImage source={{ uri: imageUrl + actor.profile_path }} />
+              <ActorName>{actor.name}</ActorName>
+              <ActorCharacter>{actor.character}</ActorCharacter>
+            </ActorWrapper>
+          ))}
+        </ActorsContainer>
+
+        {movie.homepage !== "" && (
+          <>
+            <Title>Homepage: </Title>
+            <TouchableHighlight
+              onPress={openHomepage}
+              style={{ marginBottom: 30 }}
+            >
+              <Description color={"#00f"}>{movie.homepage}</Description>
+            </TouchableHighlight>
+          </>
+        )}
+      </Content>
+    </Container>
+  );
 }
 
 export default MovieDescription;
--- pages/MovieDescription/styles.ts
+++ pages/MovieDescription/styles.ts
@@ -1,18 +1,18 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
 
 export const Container = styled.View`
   display: flex;
   align-items: center;
   justify-content: center;
   height: 100%;
-  background:  #d9d9d9;
+  background: #d9d9d9;
 `;
 
 export const Description = styled.Text`
   font-size: 16px;
   text-align: justify;
   margin-top: 5px;
-  color: ${props=>props.color?props.color:'#000'};
+  color: ${(props) => (props.color ? props.color : "#000")};
 `;
 export const Title = styled.Text`
   font-size: 18px;
@@ -54,4 +54,39 @@ export const ButtonBack = styled.TouchableHighlight`
   padding: 10px 20px;
   border-radius: 10px;
   z-index: 2;
+`;
+
+export const ActorWrapper = styled.View`
+  display: flex;
+  width: 45%;
+  height: 250px;
+  margin-left: 3%;
+  border-radius: 20px;
+  align-items: center;
+  justify-content: flex-start;
+  background: #ebebeb;
+  margin-top: 10px;
+`;
+
+export const ActorsContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-bottom: 15px;
+`;
+
+export const ActorImage = styled.Image`
+  width: 100%;
+  height: 75%;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+`;
+
+export const ActorName = styled.Text`
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const ActorCharacter = styled.Text`
+  text-align: center;
 `;
