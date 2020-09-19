7d0d7ac026b54deceadf2bbfa05bf3e1b83ecf62
Gustavo Amaro <gustavo.amaro97@gmail.com>
2020-09-19 20:42:22 -0300

    fix types

--- pages/Main/index.tsx
+++ pages/Main/index.tsx
@@ -5,11 +5,11 @@ import Header from "../../components/Header";
 import { Container } from "./styles";
 import { NavigationProps } from "../../@types";
 
-function Main({ navigation }: NavigationProps) {
+function Main({ navigation, route }: NavigationProps) {
   return (
     <Container>
       <Header />
-      <Content navigation={navigation} />
+      <Content navigation={navigation} route={route} />
     </Container>
   );
 }
